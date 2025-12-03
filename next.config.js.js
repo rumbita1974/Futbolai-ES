9ace669d4fa89e946f7656954c0f81a6706c5b4e
spice515 <adrian.g1.wso@gmail.com>
2025-12-03 19:17:33 +0100

    Final cleanup: fix file extensions, remove node installer and empty files

--- next.config.js.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['image.tmdb.org', 'img.youtube.com'],
-  },
-};
-
-module.exports = nextConfig;
